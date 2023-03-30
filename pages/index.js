293041ef757976217d63e3b932de526106b18f69
insurecar <rostyslavshyian@gmail.com>
2023-03-30 13:14:15 +0300

    addeddynamic rout

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,33 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
+import Link from "next/link";
+import Date from "../components/date";
 
 import utilStyles from "../styles/utils.module.css";
 
 import { getSortedPostsData } from "../lib/posts";
+
+export default function Home({ allPostsData }) {
+  console.log(allPostsData);
+  return (
+    <Layout home>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -12,29 +36,4 @@ export async function getStaticProps() {
       allPostsData,
     },
   };
-}
-
-export default function Home({ allPostsData }) {
-  console.log(allPostsData);
-  return (
-    <Layout home>
-      {/* Keep the existing code here */}
-
-      {/* Add this <section> tag below the existing <section> tag */}
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
-            </li>
-          ))}
-        </ul>
-      </section>
-    </Layout>
-  );
 }
